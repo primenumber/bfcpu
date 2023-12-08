862664008b6adf9b455a4b2eaed0e14198037698
primenumber <prime@kmc.gr.jp>
2023-12-09 02:38:23 +0900

    Refine test

--- src/test/scala/bfcpu/TopSpec.scala
+++ src/test/scala/bfcpu/TopSpec.scala
@@ -33,32 +33,15 @@ class TopSpec extends AnyFreeSpec with ChiselScalatestTester {
       top.io.ctrl.reset.poke(true.B)
       top.clock.step(10)
       top.io.ctrl.reset.poke(false.B)
-      println("ready: " + top.io.ctrl.ready.peek().litValue)
       top.clock.step(10)
-      println("ready: " + top.io.ctrl.ready.peek().litValue)
       top.io.ctrl.ready.expect(true.B)
       top.io.ctrl.start.poke(true.B)
       top.clock.setTimeout(0)
 
-      for (a <- 1 to 1001) {
-        println(
-          "pc: " + top.io.status.inst_addr
-            .peek()
-            .litValue + ", inst: " + top.io.status.inst
-            .peek()
-            .litValue + ", dp: " + top.io.status.data_addr
-            .peek()
-            .litValue + ", val: " + top.io.status.data
-            .peek()
-            .litValue + ", bc: " + top.io.status.bracket_count.peek().litValue
-        )
-        top.clock.step()
-      }
       top.clock.step(40000)
       top.io.ctrl.finished.expect(true.B)
-      println("finished")
+      println("execution finished")
       top.io.out.ready.poke(true.B)
-      // val out_seq = Seq(0x41.U, 0x20.U, 0x31.U, 0x20.U, 0x32.U, 0x0a.U, 0x42.U, 0x20.U, 0x31.U, 0x20.U)
       val out_seq = Seq(
         0x41.U,
         0x20.U,
@@ -149,14 +132,14 @@ class TopSpec extends AnyFreeSpec with ChiselScalatestTester {
         0x32.U,
         0x20.U,
         0x33.U,
-        0x0a.U
+        0x0a.U,
       )
       for (ex <- out_seq) {
-        println("out: " + top.io.out.bits.peek().litValue)
         top.io.out.valid.expect(true.B)
-        // top.io.out.bits.expect(ex)
+        top.io.out.bits.expect(ex)
         top.clock.step()
       }
+      top.io.out.valid.expect(false.B)
     }
   }
 }
